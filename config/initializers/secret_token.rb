50144d84ffd7d8ef3b2c6684c2aad042988f58c6
Philip Poots <philip.poots@gmail.com>
2013-03-16 20:21:39 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmberWatch::Application.config.secret_key_base = 'b67ae9104169ddbe53a6137e2dc251084b2ae357e66cf919cd6884ae30c828d13a4edcdf2c7b8bd2aea69fc7fec88100e9e85bbeded7eaffd6e4d5751d3a801b'
