ff346d5dba0db20484623dad32432b9e78651d66
Philip Poots <philip.poots@gmail.com>
2013-04-21 17:30:35 +0100

    Serve JSON at root URL

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ EmberWatch::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root to: 'welcome#index'
+  root to: 'root#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
