f801f73f7764398e334f09e65c86611915745872
Philip Poots <philip.poots@gmail.com>
2013-05-02 14:41:34 +0100

    Upgrade to Rails 4.0.0.rc1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-EmberWatch::Application.config.session_store :encrypted_cookie_store, key: '_ember_watch_session'
+EmberWatch::Application.config.session_store :cookie_store, key: '_ember_watch_session'
